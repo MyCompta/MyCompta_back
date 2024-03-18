5e9c9ae9e191dd348ff959251d5d3d6437e98ed7
totycos <dv94@hotmail.fr>
2024-03-18 16:12:01 +0100

    add invoces to seed + invoices routes moved in api/

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :invoices
+
   scope :api, defaults: { format: :json } do
       devise_for :users, controllers: {
         sessions: 'api/users/sessions',
         registrations: 'api/users/registrations'
       }
+      resources :invoices
   end
 
   # scope :api, defaults: { format: :json } do
