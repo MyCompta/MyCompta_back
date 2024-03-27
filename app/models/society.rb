ce5bdfaf3cd3ec74fd8c5331fa01f17f95f6b980
mathieu.amacher <amacher_mathieu@hotmail.com>
2024-03-27 15:23:52 +0100

    validation model society

--- app/models/society.rb
+++ app/models/society.rb
@@ -3,14 +3,14 @@ class Society < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :clients, dependent: :destroy
 
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :zip, presence: true
-  validates :city, presence: true
-  validates :country, presence: true
+  validates :name, presence: true, length: { in: 2..50 }
+  validates :address, presence: true, length: { in: 2..50 }
+  validates :zip, presence: true, length: { in: 3..10 }
+  validates :city, presence: true, length: { in: 2..60 }
+  validates :country, presence: true, length: { in: 3..60 }
   validates :siret, presence: true, numericality: { only_integer: true}, uniqueness: true
   validates :status, presence: true
-  validates :capital, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :capital, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "format is invalid" }
   validates :user_id, presence: true
 
