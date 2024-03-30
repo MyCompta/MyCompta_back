3e56163e1faf22b1782d4f8bd2955b5c2b267c79
LoulergueC <clement@loulergue.fr>
2024-03-30 09:42:00 +0000

    improved register payment_method validates

--- app/models/register.rb
+++ app/models/register.rb
@@ -6,7 +6,7 @@ class Register < ApplicationRecord
   delegate :user, to: :society
 
   validates :title, presence: true, length: { maximum: 255, minimum: 3 }
-  validates :payment_method, presence: true
+  validates :payment_method, presence: true, inclusion: { in: %w[cash card cheque transfer other] }
   validates :paid_at, presence: true
   validates :amount, presence: true
   validates :is_income, inclusion: { in: [true, false] }
