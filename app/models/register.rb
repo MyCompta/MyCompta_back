a3627ebce659ad678abb3709c22c400837f2a8c5
LoulergueC <clement@loulergue.fr>
2024-03-30 10:22:14 +0000

    updated tests with validation

--- app/models/register.rb
+++ app/models/register.rb
@@ -9,5 +9,5 @@ class Register < ApplicationRecord
   validates :payment_method, presence: true, inclusion: { in: %w[cash card cheque transfer other] }
   validates :paid_at, presence: true
   validates :amount, presence: true
-  validates :is_income, inclusion: { in: [true, false] }
+  validates :is_income, presence: true, inclusion: { in: [true, false] }
 end
--- spec/models/register_spec.rb
+++ spec/models/register_spec.rb
@@ -30,10 +30,11 @@ RSpec.describe Register, type: :model do
       expect(register).to be_valid
     end
 
-    it { should validate_presence_of(:society) }
+    it { should belong_to(:society) }
     it { should validate_presence_of(:title) }
     it { should validate_length_of(:title).is_at_least(3).is_at_most(255) }
     it { should validate_presence_of(:payment_method) }
+    it { should validate_inclusion_of(:payment_method).in_array(%w[card cash transfer cheque other]) }
     it { should validate_presence_of(:paid_at) }
     it { should validate_presence_of(:amount) }
     it { should validate_presence_of(:is_income) }
